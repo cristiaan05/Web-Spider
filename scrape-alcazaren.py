cd3272713a68059375c44d839cc283f245560435
cristiaan05 <36650269+cristiaan05@users.noreply.github.com>
2018-12-11 10:14:15 -0600

    Update scrape-alcazaren.py

--- scrape-alcazaren.py
+++ scrape-alcazaren.py
@@ -4,24 +4,36 @@
 
 import scrapy, logging
 from scrapy.http import FormRequest
+from scrapy.selector import HtmlXPathSelector
 from scrapy.contrib.spiders import CrawlSpider, Rule
+
+
 
 class KSpider(scrapy.Spider):
     name = 'alcazaren-spider'
-    start_urls = ['https://alcazaren.com.gt/catalogo-alcazaren/']
+    start_urls = ['https://alcazaren.com.gt/catalogo-alcazaren']
     handle_httpstatus_list = [400, 302]
     def parse(self, response):
-        #formdata = {'log': 'chtello05@gmail.com','pwd': 'Kemik2018' }
-        return [FormRequest(url="https://alcazaren.com.gt/catalogo-alcazaren", 
-            cookies={'edad_valida':'true'},
-            formdata={'user_login-9': 'chtello05', 'user_pass-9': 'Kemik2018'},
-            callback=self.parse_categories)]
-    def parse_categories(self, response):
+        return [FormRequest(url="https://alcazaren.com.gt/wp-login.php", cookies={'edad_valida':'true'}, callback=self.parse_login)]
+
+    def parse_login(self, response):
+        print('Si entrto')
+        #key= response.css('input[name="security"]::attr(value)').extract_first()
+        return [FormRequest(url='https://alcazaren.com.gt/wp-login.php', 
+        formdata={'log': 'chtello05@gmail.com', 'pwd': 'Kemik2018','testcookies':'1'},
+        method='POST', 
+        callback=self.parse_afterlogin)] 
+
+    def parse_afterlogin(self, response):
+        print('HOlaaaa')
+        yield scrapy.Request('https://alcazaren.com.gt/catalogo-alcazaren',callback=self.parse_categories)
+
+    def parse_categories (self, response):
+        print('entre al catalogo')
         for category in response.css('.menu-categorias-container ul li'):
             link = category.css('a::attr("href")').extract()[0]
             cat_name = category.css('a::text').extract()[0]
             yield scrapy.Request(link,callback=self.parse_products)
-            print('Login',link)
 
     def parse_products(self, response):
         for page in response.css('.pagination a::attr("href")').extract():
@@ -32,30 +44,27 @@ class KSpider(scrapy.Spider):
             yield scrapy.Request(link,callback=self.parse_description, meta={'category':prod_name})         
     
     def parse_description(self, response):
-            prod_desc = ' '.join(response.css('div.product-description > div > p::text').extract())
-            name = ' '.join(response.css('div.product-title > p.prin::text').extract())
-            sec = ' '.join(response.css('div.product-title > p.sec::text').extract())
-            terc = ' '.join(response.css('div.product-title > p.terc::text').extract())
+            prod_desc = ''.join(response.css('div.product-description > div > p::text').extract())
+            name = ''.join(response.css('div.product-title > p.prin::text').extract())
+            sec = ''.join(response.css('div.product-title > p.sec::text').extract())
+            terc = ''.join(response.css('div.product-title > p.terc::text').extract())
             sku = ' '.join(response.css('div.product_meta > span.sku_wrapper > span::text').extract())
             category = ' '.join(response.css('div.product_meta > span.posted_in > a::text').extract())
-            tags = '  '.join(response.css('div.product_meta > span.tagged_as > a::text').extract())
+            tags = ' '.join(response.css('div.product_meta > span.tagged_as > a::text').extract())
             img = '  '.join(response.css('div.product-single-image img::attr("src")').extract())
-            price = ' '.join(response.css('div.summary entry-summary >span.price nottached >span::text').extract())
-            #img = ' '.join(response.css('a.product-mask > div.product-thumb > img.attachment-shop_catalog size-shop_catalog wp-post-image animated fadeIn'))
+            price = ' '.join(response.css('span > span::text').extract()[1])
+            stock= ' '.join(response.css('span.stock_class2::text').extract())
             for  item in prod_desc:
-                item = item.encode('utf-8')     
+                item = item.encode('utf-8')        
             yield{
-                'categorias': prod_desc.replace('\n',''),
+                'categorias': prod_desc.replace('\n', ''),
                 'name': name.replace('\n', ''),
-                'sec': sec.replace('\n',''),
+                'sec': sec.replace('\n', ''),
                 'terc': terc.replace('\n', ''),
                 'sku': sku.replace('\n', ''),
                 'category': category.replace('\n', ''),
                 'tags': tags.replace('\n', ''),
                 'img': img.replace('\n', ''),
                 'price': price.replace('\n', ''),                     
+                'stock': stock.replace('\n', ''),                          
             }
-            #if stock_string == 'Disponible en Stock':
-            #stock = True
-
-        
